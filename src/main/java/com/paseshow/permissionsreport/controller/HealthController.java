4e6b12037a1fff7748c7d51da428a66ca68fc252
Roger Bariles <rogerbariles@gmai.com>
2023-02-16 13:55:03 -0300

    [DES-171]: fix HealthController

--- src/main/java/com/paseshow/permissionsreport/controller/HealthController.java
+++ src/main/java/com/paseshow/permissionsreport/controller/HealthController.java
@@ -1,7 +1,6 @@
 package com.paseshow.permissionsreport.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,6 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/")
 public class HealthController {
 
-    @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
     @GetMapping
     public Mono<ResponseEntity> health() {
         return Mono.just(ResponseEntity.ok().build());
